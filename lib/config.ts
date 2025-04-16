4baf49e62de6801786ed5a1c3bee5fe3f3dfe182
hardik-thapar <hardikthapar1@gmail.com>
2025-04-16 09:36:06 +0530

    Changed Config

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 // API configuration
-export const API_BASE_URL = "https://24eb-2409-4090-d032-9001-e472-5891-a67b-ee22.ngrok-free.app";
+export const API_BASE_URL = "https://a0d2-2401-4900-8143-d5d-7161-66c2-58ce-c28.ngrok-free.app";
 
 // API endpoints
 export const API_ENDPOINTS = {
